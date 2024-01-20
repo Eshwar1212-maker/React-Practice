eef77b387727efe15a7ee71a09db6119e912da45
Eshwar Tangirala <eshwartangirala11@gmail.com>
2024-01-20 13:32:46 -0500

    adding problem description in README for the second code challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import DigitalClock from './challenges/DigitalClock'
+
 interface AppProps {
   
 }
---
+++ src/challenges/HolyGrail.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+interface HolyGrailProps {
+  
+}
+const HolyGrail: FC<HolyGrailProps> = ({
+  
+}) => {
+  return (
+    <div>
+      HolyGrail
+    </div>
+  )
+}
+
+export default HolyGrail
